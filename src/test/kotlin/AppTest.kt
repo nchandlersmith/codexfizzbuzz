ff2a61c29ca93e3f3c208021d6b340eddd051de7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-02-13 13:21:31 -0600

    Use Kotlin skeleton with example that runs

--- src/test/kotlin/AppTest.kt
+++
@@ -1,8 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-
-class AppTest : StringSpec({
-    "returns an empty string" {
-        app() shouldBe ""
-    }
-})
