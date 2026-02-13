1994b37fdca01d73ac64b5fa3100a59c06e8762e
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-02-13 15:17:40 -0600

    Refactor fizzbuzz implementation to use string concatenation approach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 package org.example
 
 fun fizzbuzz(n: Int): String {
-    return when {
-        n % 3 == 0 && n % 5 == 0 -> "FizzBuzz"
-        n % 3 == 0 && n % 5 != 0 -> "Fizz"
-        n % 5 == 0 && n % 3 != 0 -> "Buzz"
-        else -> n.toString()
-    }
+    val fizz = if (n % 3 == 0) "Fizz" else ""
+    val buzz = if (n % 5 == 0) "Buzz" else ""
+    return if (fizz.isNotEmpty() || buzz.isNotEmpty()) fizz + buzz else n.toString()
 }
